294733c0c4304cf5aa9ccfb942587c7ba926d798
CrystalSplitter <gamewhizzit@gmail.com>
2025-01-26 19:18:09 -0800

    Define user account

--- configuration.nix
+++ configuration.nix
@@ -22,6 +22,24 @@
     neovim # Change to your favourite tiny text editor.
   ];
 
+  users.users.lixy = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    openssh = {
+      # NOTE: Change this to whatever public key you use!
+      authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINmzFzWOf+ddSaL0haZVVUn5tKjE+XsMEDGn/J4Etkuj"
+      ];
+    };
+  };
+
+  # Passwordless sudo.
+  # WARNING!
+  # If you decide to change this, remember you NEED to set a password
+  # for the chosen user with an "authorizedKeys" setting. Passwords are
+  # public in the nix store, so know what you're doing!
+  security.sudo.wheelNeedsPassword = false;
+
   # Set this to whichever system state version you're installing now.
   # Afterwards, don't change this lightly. It doesn't need to change to
   # upgrade.
