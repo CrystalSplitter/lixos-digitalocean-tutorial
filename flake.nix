b64cb5b38efaaa583d8c7c52338aa09327e0e796
CrystalSplitter <gamewhizzit@gmail.com>
2025-01-26 20:11:07 -0800

    nixos-generator flake input

--- flake.nix
+++ flake.nix
@@ -5,6 +5,10 @@
     # NOTE: Change the SHA to "nixos-unstable" for your version. I try to pin things
     # to known working commits whenever possible.
     nixpkgs.url = "github:NixOS/nixpkgs/6eac218f2d3dfe6c09aaf61a5bfa09d8aa396129";
+    nixos-generator = {
+      url = "github:nix-community/nixos-generators/d002ce9b6e7eb467cd1c6bb9aef9c35d191b5453";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
   outputs =
     { self, nixpkgs, ... }@flakeInputs:
