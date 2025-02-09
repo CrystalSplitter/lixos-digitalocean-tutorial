2bdf7f6f5a2e5d8f17929423df571b5ae463ef14
CrystalSplitter <gamewhizzit@gmail.com>
2025-02-08 19:29:16 -0800

    Add DigitalOcean config imports

--- configuration.nix
+++ configuration.nix
@@ -1,10 +1,14 @@
 # ./configuration.nix
-{ pkgs, ... }:
+{ lib, modulesPath, pkgs, ... }:
 
 {
 
   imports = [
     ./networking.nix
+  ]
+  # Required for Digital Ocean droplets.
+  ++ lib.optional (builtins.pathExists ./do-userdata.nix) ./do-userdata.nix ++ [
+    (modulesPath + "/virtualisation/digital-ocean-config.nix")
   ];
 
   # Enable flakes.
